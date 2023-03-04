950f6e0e4fcdac058965229d40d60436f579adb0
Aditya JP <adityajai243@gmail.com>
2023-03-05 01:07:24 +0530

    Create mergeKlists.py

---
+++ WEEK-6/GRADED/mergeKlists.py
@@ -0,0 +1,22 @@
+def partition(array, low, high):
+  pivot = array[high]
+  i = low - 1
+  for j in range(low, high):
+    if array[j] <= pivot:
+      i = i + 1
+      (array[i], array[j]) = (array[j], array[i])
+  (array[i + 1], array[high]) = (array[high], array[i + 1])
+  return i + 1
+
+def quickSort(array, low, high):
+  if low < high:
+    pi = partition(array, low, high)
+    quickSort(array, low, pi - 1)
+    quickSort(array, pi + 1, high)
+
+def mergeKLists(listy):
+    superListy=list()
+    for l in listy:
+        superListy.extend(l)
+    quickSort(superListy,0,len(superListy)-1)
+    return superListy
